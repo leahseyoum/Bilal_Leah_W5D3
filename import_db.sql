5e4f0a449bfa99b7823465ec435eedf9584d30ac
Leah Seyoum <leahseyoum@gmail.com>
2023-03-01 11:17:05 -0800

    create tables

--- import_db.sql
+++ import_db.sql
@@ -1,30 +1,63 @@
+
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE users(
+CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL, 
-    lname TEXT NOT NULL,
+    lname TEXT NOT NULL
 );
 
-CREATE TABLE questions(
+CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    FOREIGN KEY(author) REFERENCES users(id), 
+    author INTEGER,
+
+    FOREIGN KEY (author) REFERENCES users(id) 
 );
 
-CREATE TABLE question_follows(
+CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
-    FOREIGN KEY (questions_id) REFERENCES questions(author)
+    questions_id INTEGER,
+    users_id INTEGER,
+    
+    FOREIGN KEY (questions_id) REFERENCES questions(id),
+    FOREIGN KEY (users_id) REFERENCES users(id)
 );
 
 CREATE TABLE replies (
     id INTEGER PRIMARY KEY,
+    parent_reply INTEGER,
+    subject_question TEXT,
+    question_owner INTEGER,
+    reply_body TEXT,
+
     FOREIGN KEY (parent_reply) REFERENCES replies(id),
     FOREIGN KEY (subject_question) REFERENCES questions(title),
     FOREIGN KEY (question_owner) REFERENCES users(id),
-    FOREIGN KEY (reply_body) REFERENCES questions(body),
+    FOREIGN KEY (reply_body) REFERENCES questions(body)
 );
 
+CREATE TABLE question_like (
+    id INTEGER PRIMARY KEY,
+    user_name INTEGER,
+    question INTEGER,
 
+    FOREIGN KEY (user_name) REFERENCES users(id),
+    FOREIGN KEY (question) REFERENCES question(id)
+);
+
+INSERT INTO 
+    users (fname, lname)
+VALUES
+    ('John', 'Bob'),
+    ('Tim', 'Smith'),
+    ('Jake', 'Rob');
+
+INSERT INTO
+    questions(title, body)
+VALUES
+    ('Question?', 'zzzzzzxxxxxyyyyyy'),
+    ('QQuestion?', 'aaaaaaaaabbbbbbb'),
+    ('QQQuestion?', 'bbbbbbbbbwwwwww');
 
